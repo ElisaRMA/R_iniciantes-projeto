715cb24a5e207b5085e584e6414d238eccab4a9f
Elisa <elisarm.antunes@gmail.com>
2019-04-26 23:40:44 -0300

    Script projeto Bibliometria

---
+++ Script projeto.R
@@ -0,0 +1,8 @@
+## Script projeto Bibliometrix
+
+#pacotes
+install.packages(“bibliometrix”, dependencies=TRUE) ### installs bibliometrix package and dependencies
+library(bibliometrix)   ### load bibliometrix package
+
+#Aquisição dos dados
+
